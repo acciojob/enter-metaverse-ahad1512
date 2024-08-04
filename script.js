6b37f7c529a318011c57832518178c783ed22dab
ahad1512 <139266271+ahad1512@users.noreply.github.com>
2024-08-04 19:02:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const para = document.createElement("p");
+para.id = "status";
+para.innerText = "Enter the Metaverse";
+
+const heading = document.createElement("h1");
+heading.innerText= "Entered Multiverse";
+heading.style.display ="none";
+const button = document.createElement("button");
+button.innerText= "Enter";
+
+const body = document.getElementsByTagName("body")[0];
+
+button.addEventListener("click", function() {
+	para.style.display= "none";
+	heading.style.display ="flex";
+})
+
+body.append(para,heading,button);
